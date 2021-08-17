50f2b2eb50d97b883623af709f1771d4b75df84f
kebba <kebbaman@gmail.com>
2021-08-17 19:22:08 +0200

    Created main form, Form1, using Krypton Toolkits
    Added Krypton Toolkit references to the project and created a KryptonButton that will load the IpScan UserControl

--- NetUtilities/Form1.Designer.cs
+++ NetUtilities/Form1.Designer.cs
@@ -29,12 +29,86 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.kryptonPalette1 = new ComponentFactory.Krypton.Toolkit.KryptonPalette(this.components);
+            this.leftPanel = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.containerPanel = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.ipScanButton = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPanel)).BeginInit();
+            this.leftPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.containerPanel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // leftPanel
+            // 
+            this.leftPanel.Controls.Add(this.ipScanButton);
+            this.leftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.leftPanel.Location = new System.Drawing.Point(0, 0);
+            this.leftPanel.Name = "leftPanel";
+            this.leftPanel.Size = new System.Drawing.Size(100, 461);
+            this.leftPanel.StateCommon.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.leftPanel.StateCommon.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.leftPanel.TabIndex = 0;
+            // 
+            // containerPanel
+            // 
+            this.containerPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.containerPanel.Location = new System.Drawing.Point(100, 0);
+            this.containerPanel.Name = "containerPanel";
+            this.containerPanel.Size = new System.Drawing.Size(734, 461);
+            this.containerPanel.StateCommon.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.containerPanel.StateCommon.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.containerPanel.TabIndex = 1;
+            // 
+            // ipScanButton
+            // 
+            this.ipScanButton.Location = new System.Drawing.Point(0, 0);
+            this.ipScanButton.Name = "ipScanButton";
+            this.ipScanButton.Size = new System.Drawing.Size(100, 100);
+            this.ipScanButton.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.ipScanButton.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.ipScanButton.TabIndex = 0;
+            this.ipScanButton.Values.Image = global::NetUtilities.Properties.Resources.network_image;
+            this.ipScanButton.Values.Text = "IP Scanner";
+            this.ipScanButton.Click += new System.EventHandler(this.ipScanButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.ClientSize = new System.Drawing.Size(834, 461);
+            this.Controls.Add(this.containerPanel);
+            this.Controls.Add(this.leftPanel);
+            this.Name = "Form1";
+            this.Palette = this.kryptonPalette1;
+            this.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.None;
+            this.StateCommon.Border.Rounding = 12;
+            this.StateCommon.Header.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.StateCommon.Header.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.StateCommon.Header.ButtonEdgeInset = 10;
+            this.StateCommon.Header.Content.Padding = new System.Windows.Forms.Padding(10, -1, -1, -1);
+            this.Text = "Net Utilities";
+            ((System.ComponentModel.ISupportInitialize)(this.leftPanel)).EndInit();
+            this.leftPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.containerPanel)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPalette kryptonPalette1;
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel leftPanel;
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel containerPanel;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton ipScanButton;
     }
 }
 
--- NetUtilities/Form1.cs
+++ NetUtilities/Form1.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
 
 namespace NetUtilities
 {
-    public partial class Form1 : Form
+    public partial class Form1 : KryptonForm
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void ipScanButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
